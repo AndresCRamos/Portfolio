a382b05fc7996c8825ff7868b651750620964925
Andres Camilo Ramos Romero <andrescramosr@gmail.com>
2022-04-24 00:40:45 -0500

    Slider basic logic

---
+++ js/slider.js
@@ -0,0 +1,25 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+document.getElementById("slider_prev").onclick = function (e){
+    plusDivs(-1)
+}
+
+document.getElementById("slider_next").onclick = function (e){
+    plusDivs(-1)
+}
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("card");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "flex";
+}
