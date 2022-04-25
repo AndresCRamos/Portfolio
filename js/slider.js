1f8fa5a0fae7fc8c33e7d94d48a4f3572e457e34
Andres Camilo Ramos Romero <andrescramosr@gmail.com>
2022-04-25 17:19:52 -0500

    Next slide effect

--- js/slider.js
+++ js/slider.js
@@ -6,7 +6,7 @@ document.getElementById("slider_prev").onclick = function (e){
 }
 
 document.getElementById("slider_next").onclick = function (e){
-    plusDivs(-1)
+    plusDivs(1)
 }
 
 function plusDivs(n) {
@@ -16,10 +16,22 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("project_card");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  if (n > x.length) {
+    slideIndex = 1;
   }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  prev = slideIndex-1;
+  if (prev == 0) {
+    prev = x.length
+  }
+  console.log(slideIndex);
+  console.log(prev);
+  x[slideIndex-1].classList.remove("animate-right");
   x[slideIndex-1].style.display = "flex";
+  x[slideIndex-1].classList.add("animate-left");
+  
+  x[prev-1].classList.remove("animate-left");
+  x[prev-1].classList.add("animate-right");
 }
