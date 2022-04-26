f103cca88be0bcf649a14d4c38e261be3bfd21ed
Andres Camilo Ramos Romero <andrescramosr@gmail.com>
2022-04-26 01:28:56 -0500

    Animated Horizontal Slider

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,14 @@
 var slideIndex = 1;
-showDivs(slideIndex);
+
+init();
+
+function init(){
+  Array.from(document.getElementsByClassName("project_card")).forEach(e => {
+    e.classList.add("animate-out-right")
+  })
+  showDivs(slideIndex, true);
+}
+
 
 document.getElementById("slider_prev").onclick = function (e){
     plusDivs(-1)
@@ -10,28 +19,63 @@ document.getElementById("slider_next").onclick = function (e){
 }
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+  showDivs(slideIndex += n, n>0);
 }
 
-function showDivs(n) {
-  var i;
+function showDivs(nextSlideIndex, isNext) {
   var x = document.getElementsByClassName("project_card");
-  if (n > x.length) {
-    slideIndex = 1;
+  if (nextSlideIndex > x.length) {
+    nextSlideIndex = 1;
   }
-  if (n < 1) {
-    slideIndex = x.length;
+  if (nextSlideIndex < 1) {
+    nextSlideIndex = x.length;
   }
-  prev = slideIndex-1;
-  if (prev == 0) {
-    prev = x.length
+  let prev;
+  if (isNext) {
+    prev = nextSlideIndex-1;
+    if(prev <= 0){
+      nextSlideIndex = 1;
+      prev = x.length;
+    }
   }
-  console.log(slideIndex);
-  console.log(prev);
-  x[slideIndex-1].classList.remove("animate-right");
-  x[slideIndex-1].style.display = "flex";
-  x[slideIndex-1].classList.add("animate-left");
+  else {
+    prev = nextSlideIndex+1;
+    if(prev > x.length){
+      nextSlideIndex = x.length;
+      prev = 1;
+    }
+  }
+  slideIndex = nextSlideIndex;
+  let currSlide= x[prev-1]
+  let nextSlide= x[nextSlideIndex-1]
+  if (isNext){
+    animate(currSlide, nextSlide, isNext)
+  }
+  else {
+    animate(nextSlide,currSlide, isNext)
+  }
+
   
-  x[prev-1].classList.remove("animate-left");
-  x[prev-1].classList.add("animate-right");
+  slideIndex = nextSlideIndex;
+}
+
+function animate(currSlide, nextSlide, isNext){
+  nextSlide.style.display = "flex";
+  resetAnimation(nextSlide)
+  resetAnimation(currSlide)
+  if (isNext) {
+    currSlide.classList.add("animate-out-right")
+    nextSlide.classList.add("animate-in-left")
+  }
+  if (!isNext) {
+    nextSlide.classList.add("animate-out-left")
+    currSlide.classList.add("animate-in-right")
+  }
+}
+
+function resetAnimation(el) {
+  el.classList.remove("animate-in-left")
+  el.classList.remove("animate-out-left")
+  el.classList.remove("animate-in-right")
+  el.classList.remove("animate-out-right")
 }
