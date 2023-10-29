b193f2ad2d64d124ae1d431ae9cc0ebc82449ab0
AndresCRamos <andrescramosr@gmail.com>
2023-10-29 18:59:43 -0500

    About component scaffold created

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import SnapScroll from '$lib/components/SnapScroll.svelte';
+  import About from '$lib/components/About.svelte';
+</script>
+
+<main class="p-5">
+  <SnapScroll>
+    <About />
+  </SnapScroll>
+</main>
