35b30c94b3b7e9fd408d899c722db2fff6b821a1
AndresCRamos <andrescramosr@gmail.com>
2023-10-29 18:58:34 -0500

    SnapScroll Component created

---
+++ src/lib/components/SnapScroll.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  function snapScroll(node: any) {
+    const children = [...node.children];
+    children.forEach((child) => {
+      const snap_child = document.createElement('div');
+      snap_child.className = 'snap-center flex flex-col justify-center items-center w-full h-full';
+      node.replaceChild(snap_child, child);
+      snap_child.appendChild(child);
+    });
+
+    return {
+      destroy() {}
+    };
+  }
+</script>
+
+<div class="snap-y snap-mandatory w-screen h-screen overflow-y-scroll" use:snapScroll>
+  <slot />
+</div>
