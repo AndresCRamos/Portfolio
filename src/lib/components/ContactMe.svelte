1f3783db47eb71b39f95da05126a9909451a6118
AndresCRamos <andrescramosr@gmail.com>
2023-10-29 22:25:04 -0500

    Contact me scaffolding

---
+++ src/lib/components/ContactMe.svelte
@@ -0,0 +1,8 @@
+<div>
+  <h1>Contact Me</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem sunt officiis quos odit
+    praesentium! Eligendi aut, esse magni sit error praesentium, soluta voluptas, ut quasi nemo
+    deserunt! Molestias, rerum magnam.
+  </p>
+</div>
