a89243571790d0b270721c7bd3473a482f2830a7
AndresCRamos <andrescramosr@gmail.com>
2023-10-29 22:49:53 -0500

    Project Scaffolding

---
+++ src/lib/components/Projects.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Card } from 'flowbite-svelte';
+</script>
+
+<div class="flex gap-2">
+  <Card>
+    <h1>Project 1</h1>
+  </Card>
+  <Card>
+    <h1>Project 2</h1>
+  </Card>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   import SnapScroll from '$lib/components/SnapScroll.svelte';
   import About from '$lib/components/About.svelte';
+  import ContactMe from '$lib/components/ContactMe.svelte';
+  import Projects from '$lib/components/Projects.svelte';
 </script>
 
 <main>
   <SnapScroll>
     <About />
+    <ContactMe />
+    <Projects />
   </SnapScroll>
 </main>
