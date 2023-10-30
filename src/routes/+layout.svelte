35289e74651c06b3e2b19248b64de27176ee7a71
AndresCRamos <andrescramosr@gmail.com>
2023-10-30 18:07:30 -0500

    Changes to dark theme styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
   import { DarkMode } from 'flowbite-svelte';
 </script>
 
-<div class="dark:bg-gray-800 dark:text-gray-400">
+<div>
   <nav class="flex justify-end h-[5vh]">
     <DarkMode class="w-[5vh] flex justify-center items-center" />
   </nav>
