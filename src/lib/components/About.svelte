32040994ba63f62d939849f5f9188efa5520a51e
AndresCRamos <andrescramosr@gmail.com>
2023-10-29 22:25:21 -0500

    add lorem ipsum to about

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,3 +1,8 @@
 <div>
   <h1>About me</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit cumque est quos
+    repudiandae, magnam quo officiis eos placeat non architecto temporibus nemo? Sit aliquam fugiat
+    amet fuga possimus laborum quasi.
+  </p>
 </div>
