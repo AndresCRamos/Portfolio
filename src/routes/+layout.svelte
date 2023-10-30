982ee21c008870be0be4b8c86c266987e76596c7
AndresCRamos <andrescramosr@gmail.com>
2023-10-30 00:41:16 -0500

    Minimal support for dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,9 @@
   import { DarkMode } from 'flowbite-svelte';
 </script>
 
-<nav>
-  <DarkMode />
-</nav>
-<slot />
+<div class="dark:bg-gray-800 dark:text-gray-400">
+  <nav class="flex justify-end h-[5vh]">
+    <DarkMode class="w-[5vh] flex justify-center items-center" />
+  </nav>
+  <slot />
+</div>
