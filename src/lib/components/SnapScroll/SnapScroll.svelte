0fad36090e681b47aac3ba2099fb25d3d371f9c2
AndresCRamos <andrescramosr@gmail.com>
2023-11-01 22:42:43 -0500

    Customized scrollbar

--- src/lib/components/SnapScroll/SnapScroll.svelte
+++ src/lib/components/SnapScroll/SnapScroll.svelte
@@ -1,3 +1,19 @@
-<div class="snap-y snap-mandatory overflow-y-scroll h-[95dvh]">
+<div class="snap-y snap-mandatory overflow-y-scroll h-[95dvh] custom_scrollbar">
   <slot />
 </div>
+
+<style lang="postcss">
+  /* This will change the scrollbar for the whole page */
+  .custom_scrollbar::-webkit-scrollbar {
+    @apply w-2;
+  }
+
+  .custom_scrollbar::-webkit-scrollbar-track {
+    @apply bg-none;
+  }
+
+  .custom_scrollbar::-webkit-scrollbar-thumb {
+    border-radius: 0.5rem;
+    @apply bg-gray-400 dark:bg-gray-800;
+  }
+</style>
