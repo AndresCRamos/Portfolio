1979cf80b010e6a3168989aa735f2d5f8979ec92
AndresCRamos <andrescramosr@gmail.com>
2023-11-07 17:23:11 -0500

    TextBlock component added

---
+++ src/lib/components/ExperienceBlocks/TextBlock.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import BasicBlock from './BasicBlock.svelte';
+
+  export let title: string = '';
+  export let text: string;
+</script>
+
+<BasicBlock {title}>
+  <p>{text}</p>
+</BasicBlock>
