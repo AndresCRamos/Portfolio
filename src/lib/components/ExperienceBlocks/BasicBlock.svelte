b46ec72512f80602f9838be0bf1295c01ed0912f
AndresCRamos <andrescramosr@gmail.com>
2023-11-07 16:10:54 -0500

    Extracted resusable logic to BasicBlock Component

---
+++ src/lib/components/ExperienceBlocks/BasicBlock.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let title: string = '';
+</script>
+
+<div>
+  {#if title}
+    <h3 class="text-lg text-accent-500 dark:text-accent-300">{title}</h3>
+  {/if}
+
+  <slot />
+</div>
--- src/lib/components/ExperienceBlocks/ListBLock.svelte
+++ src/lib/components/ExperienceBlocks/ListBLock.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
+  import BasicBlock from './BasicBlock.svelte';
+
   export let title: string = '';
   export let value: any[];
 </script>
 
-{#if title}
-  <h3 class="text-lg text-accent-500 dark:text-accent-300">{title}</h3>
-{/if}
-
-<ul>
-  {#each value as item}
-    <li class="font-light">{item}</li>
-  {/each}
-</ul>
+<BasicBlock {title}>
+  <ul>
+    {#each value as item}
+      <li class="font-light">{item}</li>
+    {/each}
+  </ul>
+</BasicBlock>
