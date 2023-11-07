20c6f5a73fc1c31e13aa0c1c7c538f1f4144db13
AndresCRamos <andrescramosr@gmail.com>
2023-11-07 18:16:05 -0500

    In depth description for MercadoLibre job

--- src/routes/jobs/[experiences]/+page.ts
+++ src/routes/jobs/[experiences]/+page.ts
@@ -22,6 +22,17 @@ const job_experience = new Map([
           type: 'list',
           title: 'Tech Stack',
           value: ['Golang', 'Gin Gonic', 'REST APIs']
+        },
+        {
+          type: 'text',
+          title: 'In depth',
+          value:
+            'In Mercado Libre i first worked maintaining the microservices responsible for processing payments made using credit or debit cards'
+        },
+        {
+          type: 'text',
+          value:
+            'Then i worked in the Business Continuous Integration, helping with solving more complex problems, still related to the card payments ecosystem'
         }
       ] as Block[]
     }
