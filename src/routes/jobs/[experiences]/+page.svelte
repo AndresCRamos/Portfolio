8b53d167be5bf977575f978798e5bbde4cde0573
AndresCRamos <andrescramosr@gmail.com>
2023-11-07 15:32:07 -0500

    Job Experience Route basic draft added

---
+++ src/routes/jobs/[experiences]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<h1 class="text-4xl">{data.title}</h1>
+<h2 class="text-2xl text-secondary-500 dark:text-secondary-400">{data.position}</h2>
+{#if data.stack}
+  <h3 class="text-2xl text-accent-500 dark:text-accent-300">Tech stack</h3>
+  <ul>
+    {#each data.stack as stack}
+      <li>{stack}</li>
+    {/each}
+  </ul>
+{/if}
---
+++ src/routes/jobs/[experiences]/+page.ts
@@ -0,0 +1,22 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load = (async ({ params }) => {
+  const experience = params.experiences;
+  if (!job_experience.has(experience)) {
+    throw error(404, 'job_not_exists');
+  }
+  const experienceData = job_experience.get(experience);
+  return experienceData;
+}) satisfies PageLoad;
+
+const job_experience = new Map([
+  [
+    'mercadolibre',
+    {
+      title: 'Mercado Libre',
+      position: 'Software Analyst',
+      stack: ['Golang', 'Gin Gonic', 'REST APIs']
+    }
+  ]
+]);
