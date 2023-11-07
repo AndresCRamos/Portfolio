abf2e546e89e0970475234c7d5c081eeda0ab908
AndresCRamos <andrescramosr@gmail.com>
2023-11-07 16:11:16 -0500

    DynamicBLocks component added

---
+++ src/lib/components/ExperienceBlocks/DynamicBlocks.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import ListBLock from './ListBLock.svelte';
+
+  interface block {
+    type: string;
+    title: string | undefined;
+    value: any;
+  }
+
+  export let block_list: block[];
+</script>
+
+{#each block_list as block}
+  {#if block.type == 'list'}
+    <ListBLock title={block.title} value={block.value} />
+  {/if}
+{/each}
--- src/routes/jobs/[experiences]/+page.svelte
+++ src/routes/jobs/[experiences]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import ListBLock from '$lib/components/ExperienceBlocks/ListBLock.svelte';
+  import DynamicBlocks from '$lib/components/ExperienceBlocks/DynamicBlocks.svelte';
   import type { PageData } from './$types';
 
   export let data: PageData;
@@ -8,9 +8,5 @@
 <h1 class="text-4xl">{data.title}</h1>
 <h2 class="text-2xl text-secondary-500 dark:text-secondary-400">{data.position}</h2>
 {#if data.blocks}
-  {#each data.blocks as block}
-    {#if block.type == 'list'}
-      <ListBLock title={block.title} value={block.value} />
-    {/if}
-  {/each}
+  <DynamicBlocks block_list={data.blocks} />
 {/if}
