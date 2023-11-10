da10d10d4dff78ce148d7d5bdeec4dea65f688cd
AndresCRamos <andrescramosr@gmail.com>
2023-11-09 23:01:08 -0500

    Changed scrollbar styles to global

--- src/lib/components/SnapScroll/SnapScroll.svelte
+++ src/lib/components/SnapScroll/SnapScroll.svelte
@@ -1,19 +1,3 @@
-<div class="snap-y snap-mandatory overflow-y-scroll h-[95dvh] custom_scrollbar">
+<div class="snap-y snap-mandatory overflow-y-scroll h-[95dvh]">
   <slot />
 </div>
-
-<style lang="postcss">
-  /* This will change the scrollbar for the whole page */
-  .custom_scrollbar::-webkit-scrollbar {
-    @apply w-2;
-  }
-
-  .custom_scrollbar::-webkit-scrollbar-track {
-    @apply bg-none;
-  }
-
-  .custom_scrollbar::-webkit-scrollbar-thumb {
-    border-radius: 0.5rem;
-    @apply bg-gray-400 dark:bg-gray-800;
-  }
-</style>
