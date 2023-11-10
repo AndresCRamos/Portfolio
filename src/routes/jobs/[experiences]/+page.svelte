8d087bd0dec7642d3938af912b8207152152ec8b
AndresCRamos <andrescramosr@gmail.com>
2023-11-09 21:25:00 -0500

    Responsive margins added

--- src/routes/jobs/[experiences]/+page.svelte
+++ src/routes/jobs/[experiences]/+page.svelte
@@ -11,7 +11,9 @@
   <div class=" w-4/5 mx-auto mt-24 mb-8">
     <Button href="/" size="lg" class="w-full max-w-3xl">Go back</Button>
     <h1 class="text-4xl mt-12">{data.title}</h1>
-    <h2 class="text-2xl text-secondary-500 dark:text-secondary-400 mb-24">{data.position}</h2>
+    <h2 class="text-2xl text-secondary-500 dark:text-secondary-400 mb-6 md:mb-12">
+      {data.position}
+    </h2>
     {#if data.blocks}
       <DynamicBlocks block_list={data.blocks} />
       <div class="mb-24 md:mb-32" />
