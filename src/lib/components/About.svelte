c05a0b6a5ee8bdbe684d2815fbd8a8a80b268e19
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2023-11-17 16:09:50 -0500

    Better spacing between main About me section elements

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -6,19 +6,16 @@
 <div class="flex flex-col gap-2">
   <h1>About me</h1>
   <h2 class="font-medium">Hi im Andres Ramos</h2>
-  <p>For years i have studied the almost magic ways of making computers do cool stuff</p>
-  <p>A full stack dev starting its new journey in this space full opportunities and challenges</p>
-  <p>
-    Im a passionate learner, always searching for new ways to improve my skills and ways to
-    opportunities to challenge myself in the search of improving
-  </p>
+  <div class="flex flex-col gap-2 my-4">
+    <p>For years i have studied the almost magic ways of making computers do cool stuff</p>
+    <p>A full stack dev starting its new journey in this space full opportunities and challenges</p>
+    <p>
+      Im a passionate learner, always searching for new ways to improve my skills and ways to
+      opportunities to challenge myself in the search of improving
+    </p>
+  </div>
+
   <div>
     <Button href="{base}/about">More about me</Button>
   </div>
 </div>
-
-<style lang="postcss">
-  p {
-    @apply my-2;
-  }
-</style>
