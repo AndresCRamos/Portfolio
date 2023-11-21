a2048d7dcdb1cc937542e8e1322f61860d3c187d
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2023-11-21 17:30:58 -0500

    Add Content Arrow indicators

--- src/lib/components/SnapScroll/SnapChild.svelte
+++ src/lib/components/SnapScroll/SnapChild.svelte
@@ -1,5 +1,15 @@
+<script>
+  import { AngleDownSolid, AngleUpSolid } from 'flowbite-svelte-icons';
+</script>
+
 <div
-  class="mx-auto snap-center flex flex-col justify-center items-center w-full max-w-4xl h-full pl-5 pr-3"
+  class="relative mx-auto snap-center flex flex-col justify-center items-center w-full max-w-4xl h-full pl-5 pr-3 snap-child"
 >
+  <AngleUpSolid size="lg" class="snap-child-indicator-up absolute top-0 left-0 right-0 mx-auto" />
+
   <slot />
+  <AngleDownSolid
+    size="lg"
+    class="snap-child-indicator-down absolute left-0 right-0 mx-auto bottom-2"
+  />
 </div>
