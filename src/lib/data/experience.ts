1217a37b707e03d5200b0a928094c81802750240
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2023-11-21 15:42:14 -0500

    Typos on Job Experiences

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -3,14 +3,14 @@ const experiences = [
     company: 'Contractor at Tata Consultant Services',
     job: 'PHP Junior Dev',
     description:
-      'I worked for a software engineer who had a contract with Tata Consultant Services, mantaining PHP legacy apps for clients',
+      'I worked for a software engineer who had a contract with Tata Consultant Services, maintaining PHP legacy apps for clients',
     redirect: 'jobs/tcs'
   },
   {
     company: 'Mercado pago',
     job: 'Software Analyst',
     description:
-      'I worked with the communication layer between Mercado Pago ecosystem and banks accross Latin America',
+      'I worked with the communication layer between Mercado Pago ecosystem and banks across Latin America',
     redirect: 'jobs/mercadolibre'
   }
 ];
