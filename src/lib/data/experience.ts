f2531db0eb7d32ca3a0a22cee90a107096279aab
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2023-11-21 15:57:51 -0500

    Small Changes to Jobs experiences cards

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -3,14 +3,14 @@ const experiences = [
     company: 'Contractor at Tata Consultant Services',
     job: 'PHP Junior Dev',
     description:
-      'I worked for a software engineer who had a contract with Tata Consultant Services, maintaining PHP legacy apps for clients',
+      'I worked for a software engineer who had a contract with Tata Consultant Services, there i maintained and improved PHP apps for clients',
     redirect: 'jobs/tcs'
   },
   {
     company: 'Mercado pago',
     job: 'Software Analyst',
     description:
-      'I worked with the communication layer between Mercado Pago ecosystem and banks across Latin America',
+      'I worked in the team that maintained and improved the communication layer between Mercado Pago payment ecosystem and banks across Latin America',
     redirect: 'jobs/mercadolibre'
   }
 ];
